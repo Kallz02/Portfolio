b6120b297a267b06a35eb6cc3cef4fcf1477c398
Kallz02 <akshay.pranav.kalathil@gmail.com>
2023-06-22 22:59:24 +0530

    update

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,4 +1,19 @@
-<!-- blog/+page.svelte -->
+<script>
+	import PostsList from '$lib/components/PostsList.svelte'
+	import Pagination from '$lib/components/Pagination.svelte'
+	import { siteDescription } from '$lib/config'
+
+	export let data
+</script>
+
+
+<svelte:head>
+	<title>Blog</title>
+	<meta data-key="description" name="description" content={siteDescription}>
+</svelte:head>
+
 <h1>Blog</h1>
 
-<p>My blog posts will go here eventually…</p>
+<PostsList posts={data.posts} />
+
+<Pagination currentPage={1} totalPosts={data.total} />
