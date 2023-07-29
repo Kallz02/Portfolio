28e782fe5c5e2ac9213b3f12809778d9ca2ce7d5
Kallz02 <akshay.pranav.kalathil@gmail.com>
2023-07-29 06:33:47 +0530

    update plausible

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,9 @@
 	import Landing from '$lib/component/Landing/Landing.svelte';
 </script>
 
+<svelte:head>
+	<meta name="description" content="Welcome to my developer portfolio where i showcase all my work experience and projects along with blog content i post post regularyly contact me for any development services">
+
+</svelte:head>
+
 <Landing />
